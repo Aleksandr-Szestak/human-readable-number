98f42dc2c39b3237c2e5185555144720d9be4823
Aleksandr Szestak <szestak@hotmail.com>
2023-08-19 17:28:45 +0300

    refactor: improve function code

--- src/index.js
+++ src/index.js
@@ -1,36 +1,46 @@
 module.exports = function toReadable (number) {
+let res = '';
+
+if (number === 0){
+  res = 'zero';
+}
+else{
+
   const ed = ['one','two','three','four','five',
-            'six','seven','eight','nine'];
-  
-const d1 = ['eleven','twelve','thirteen','fourteen','fifteen',
-  'sixteen','seventeen','eighteen','nineteen'];
-  
-const de = ['ten','twenty','thirty','forty','fifty',
-            'sixty','seventy','eighty','ninety'];
-  
-let res = number===0?'zero':'';
-let c1=0, c2=0, c3=0;
-let a=number;
+              'six','seven','eight','nine'];
+    
+  const d1 = ['eleven','twelve','thirteen','fourteen','fifteen',
+              'sixteen','seventeen','eighteen','nineteen'];
+    
+  const de = ['ten','twenty','thirty','forty','fifty',
+              'sixty','seventy','eighty','ninety'];    
 
-c1 = a % 10;       //единицы
-a = (a-c1) / 10;
+  let c1=0, c2=0, c3=0;  
+  let a=number;
 
-c2 = a % 10;       //десятки
-a = (a-c2) / 10;
+  c1 = a % 10;       //ones
+  a = (a-c1) / 10;
 
-c3 = a % 10;       //сотни
+  c2 = a % 10;       //tens
+  a = (a-c2) / 10;
 
-if (c3 > 0)  
-  res = ed[c3-1] + ' hundred'
-  
-if (c2===1 && c1!==0)                  // от 11 до 19
-  res = res + (res===''?'':' ') + d1[c1-1];
-else if(c2 > 1 || (c2===1 && c1===0))  // 10 и далее от 20
-  res = res + (res===''?'':' ') + de[c2-1];
+  c3 = a % 10;       //hundreds
 
-if (c1>0 && c2!==1)
-  res = res + (res===''?'':' ') + ed[c1-1];
-  
-  return res;
+  if (c3 > 0){
+    res = ed[c3-1] + ' hundred';
+  }
+    
+  if (c2===1 && c1!==0){                  // from 11 to 19
+    res = res + (res===''?'':' ') + d1[c1-1];
+  }
+  else if(c2 > 1 || (c2===1 && c1===0)){  // 10 and on from 20
+    res = res + (res===''?'':' ') + de[c2-1];
+  }
 
+  if (c1>0 && c2!==1){
+    res = res + (res===''?'':' ') + ed[c1-1];
+  }
+}
+
+return res;
 }
